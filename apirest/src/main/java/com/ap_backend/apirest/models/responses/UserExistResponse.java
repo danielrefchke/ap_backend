35b644a4ef5bd32210dfcf3284ec6e92ce56625f
Daniel Refchke <xshade@gmail.com>
2023-03-12 17:50:51 -0300

    clase faltante

---
+++ apirest/src/main/java/com/ap_backend/apirest/models/responses/UserExistResponse.java
@@ -0,0 +1,12 @@
+package com.ap_backend.apirest.models.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserExistResponse {
+    private boolean exist;
+}
